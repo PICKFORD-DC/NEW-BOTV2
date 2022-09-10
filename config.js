441e939f95d4231102fe8033ec078e93ed8e79ca
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-09-10 23:15:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 let fs = require('fs') 
 let chalk = require('chalk')
 
-owner = [
+/*owner = [
   ['62895330379186'],
   ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['62895330379186', 'Owner', true]*/
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
