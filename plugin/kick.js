861c234497a0d85b385d2eb945fe5bf5bc9a40f1
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-09-11 06:16:36 +0700

    Update kick.js

--- plugin/kick.js
+++ plugin/kick.js
@@ -1,4 +1,4 @@
-*/let handler = async (m, { conn, args }) => {
+/*let handler = async (m, { conn, args }) => {
   let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
   let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupParticipantsUpdate(m.chat, [user], "remove")
@@ -18,4 +18,4 @@ handler.botAdmin = true
 handler.fail = null
 handler.limit = true
 
-module.exports = handler
+module.exports = handler*/
